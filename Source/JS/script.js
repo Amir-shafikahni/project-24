38a2595ee0f83a97d0e63e618fd0f3ba35919b56
Amir_sh <mrshfkhn@gmail.com>
2022-07-07 17:48:15 +0430

    bug fixed

--- Source/JS/script.js
+++ Source/JS/script.js
@@ -1,34 +1,42 @@
-let $ = document
+let $ = document;
 
 ////////////////////////////
 
 // variables //////////////
-const body = $.body
-const allLiElems = $.querySelectorAll("li")
-const textInput = $.querySelector(".textInput")
+const body = $.body;
+const allLiElems = $.querySelectorAll("li");
+const textInput = $.querySelector(".textInput");
 
 // fucntions //////////////////
 // the type action and setting animation
-function typeActionAndKeyAniamtion (event){
-    // to set an animation to the pressed keys
-    allLiElems.forEach(function(liElem){
-        if(liElem.id === event.key){
-            liElem.classList.add("keyAnimation")
-            setTimeout(function(){
-                liElem.classList.remove("keyAnimation")
-            },150)
-        }
-    })
+function typeActionAndKeyAniamtion(event) {
+  // to set an animation to the pressed keys
+  let userEventKey = event.key;
+  let mainKeyElem = document.getElementById(userEventKey);
 
-    // the type action
-    if(event.key === "Backspace"){
-        textInput.innerHTML = textInput.innerHTML.substring(0, textInput.innerHTML.length-1)
-    }else if(event.key === "Shift" || event.key === "Tab" || event.key === "CapsLock" || event.key ==="Enter"){
-        return
-    }else{
-        textInput.innerHTML += event.key
-    }
+  mainKeyElem.classList.add("keyAnimation");
+
+  mainKeyElem.addEventListener("animationend", function () {
+    mainKeyElem.classList.remove("keyAnimation");
+  });
+  
+  // the type action
+  if (event.key === "Backspace") {
+    textInput.innerHTML = textInput.innerHTML.substring(
+      0,
+      textInput.innerHTML.length - 1
+    );
+  } else if (
+    event.key === "Shift" ||
+    event.key === "Tab" ||
+    event.key === "CapsLock" ||
+    event.key === "Enter"
+  ) {
+    return;
+  } else {
+    textInput.innerHTML += event.key;
+  }
 }
 
 // event liseteners //////////////
-window.addEventListener("keydown" , typeActionAndKeyAniamtion)
+window.addEventListener("keydown", typeActionAndKeyAniamtion);
