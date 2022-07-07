0753a438966b930e7ea51eee7d307875219d4f47
Amir_sh <mrshfkhn@gmail.com>
2022-07-07 17:00:13 +0430

    firat commit

---
+++ Source/JS/script.js
@@ -0,0 +1,34 @@
+let $ = document
+
+////////////////////////////
+
+// variables //////////////
+const body = $.body
+const allLiElems = $.querySelectorAll("li")
+const textInput = $.querySelector(".textInput")
+
+// fucntions //////////////////
+// the type action and setting animation
+function typeActionAndKeyAniamtion (event){
+    // to set an animation to the pressed keys
+    allLiElems.forEach(function(liElem){
+        if(liElem.id === event.key){
+            liElem.classList.add("keyAnimation")
+            setTimeout(function(){
+                liElem.classList.remove("keyAnimation")
+            },150)
+        }
+    })
+
+    // the type action
+    if(event.key === "Backspace"){
+        textInput.innerHTML = textInput.innerHTML.substring(0, textInput.innerHTML.length-1)
+    }else if(event.key === "Shift" || event.key === "Tab" || event.key === "CapsLock"){
+        return
+    }else{
+        textInput.innerHTML += event.key
+    }
+}
+
+// event liseteners //////////////
+window.addEventListener("keydown" , typeActionAndKeyAniamtion)
